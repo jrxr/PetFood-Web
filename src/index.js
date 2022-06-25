dd682b427b7befaceae2b7447bd04ff4ae75c2dc
Agacy junior <juniormartinssom@gmail.com>
2022-06-25 13:33:59 -0300

    Tela de cadastro finalizada

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './styles/global.css';
 
 import Cadastro from "./pages/Cadastro";
 
--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,7 +1,54 @@
+import Illustration from "../../assets/illustration.png";
+import Header from "../../components/header";
+
 const Cadastro = () => {
   return (
-    <h1>Hello World</h1>
+    <div className="container-fluid h-100 bg-primary">
+      <Header whiteVersion />
+      <div className="row">
+        <div className="col-6 text-right my-auto">
+          <img src={Illustration} className="img-fluid" />
+        </div>
+
+        <div className="col-6">
+          <div className="box col-8">
+            <h2 className="text-center">Falta puco pra fazer o seu pet feliz.</h2>
+            <br />
+            <br />
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="Nome Completo"
+            />
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="E-mail"
+            />
+            <input
+              type="password"
+              className="form-control form-control-lg mb-3"
+              placeholder="Telefone"
+            />
+            <input
+              type="text"
+              className="form-control form-control-lg mb-3"
+              placeholder="CPF"
+            />
+            <input
+              type="date"
+              className="form-control form-control-lg mb-3"
+              placeholder="Data de Nascimento"
+            />
+
+            <button className="btn btn-lg btn-block btn-secondary">
+              Finalizar Pedido
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Cadastro;
